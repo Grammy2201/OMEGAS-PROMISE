d37178d8c69c104efb6c99072b3051efea47c7e0
Gregory Sims <gregorys234@gmail.com>
2024-08-30 21:30:33 -0500

    ADDED CSS

--- Omegaapp/src/app/app.component.ts
+++ Omegaapp/src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [
+        RouterOutlet,
+        RouterLink,
+        RouterLinkActive,
+        RouterModule,
+        CommonModule
+    ],
 })
 export class AppComponent {
-  title = 'Omegaapp';
+  title = 'OMEGAS PROMISE';
+
+  router = inject(Router)
+
+  navigate() {
+    this.router.navigate(['/home']);
+  }
+
+ navigateToDonate() {
+  this.router.navigate(['/donate']);
+ }
+
+ navigateToAbout() {
+  this.router.navigate(['/about']);
+ }
+ navigateToContact() {
+  this.router.navigate(['/contact']);
+ }
+
+
 }
--- Omegaapp/src/app/app.routes.ts
+++ Omegaapp/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './About/about.component';
+import { ContactComponent } from './Contact/contact.component';
+import { DonateComponent } from './Donate/donate.component';
+import { HomeComponent } from './Home/home.component';
 
-export const routes: Routes = [];
+
+
+
+export const routes: Routes = [
+
+    {path:'home', component:HomeComponent},
+    {path:'about', component:AboutComponent},
+    {path:'contact', component:ContactComponent},
+    {path:'donate', component:DonateComponent}
+];
